9c7d615a7c459ed69d50b085e64d6ad23958d9bd
Steve Garrity <sgarrity@gmail.com>
2022-07-25 14:10:54 -0700

    fix deploy script to push directly to lambda too

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ echo "you need to have valid AWS creds in your session, source tools/aws-mfa.sh 
 
 zip -j amazon-cloudwatch-auto-alarms.zip src/*
 
+# we push to s3 just in case because that's how the CloudFormation template loaded the orig code
 aws s3 cp amazon-cloudwatch-auto-alarms.zip s3://amazon-cloudwatch-auto-ala-lambdadeploymentbucket-1h76ytps4s5ir
+
+# this seems to be easier to get lambda to load the code
+aws lambda update-function-code --function-name CloudWatchAutoAlarms --zip-file fileb://amazon-cloudwatch-auto-alarms.zip
 
 echo "done...I think"
